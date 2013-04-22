db7fc898f3d6ba41dd32063ee0d12fbab4057a22
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-04-22 20:29:19 +0200

    jquery mobile and first interaction

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-$(function() { 
-	$("#auth").click(function() { 
-		
-	}); 
+var EvImages = {
+	goal: 'http://cdn1.iconfinder.com/data/icons/ballcons/png/classic.png',
+	ocasion: 'http://cdn1.iconfinder.com/data/icons/ballcons/png/classic.png'
+};
+
+$(document).ready(function() {
+  	// disable ajax nav
+  	$.mobile.ajaxLinksEnabled = false;
+});
+
+var saveUserToken = function() {
+	var token = $('input#user_token').val();
+	localStorage['userToken'] = token;
+};
+
+$(document).on('pageinit', '#page1', function(){
+
+});
+
+$(document).on('pageinit', '#page2', function(){
+    $('#match').text(localStorage['userToken']);
 });
