37c040b0c34acb08a4b0500bcfb65048661d3f81
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-04-23 16:51:29 +0200

    code simplificated

--- js/app.js
+++ js/app.js
@@ -3,32 +3,57 @@ var EvImages = {
 	ocasion: 'http://cdn1.iconfinder.com/data/icons/ballcons/png/classic.png'
 };
 
+var response = null;
+
 var saveUserToken = function() {
 	var token = $('input#user_token').val();
 	localStorage['userToken'] = token;
 };
 
+var updateLive = function() {};
+
+var buildMatch = function() {
+	if (!response) return null;
+
+	return '2'+' '+'no-suelo'+' : '+'opponent team'+' '+'1';
+};
+
 $(document).ready(function() {
   	// disable ajax nav
   	$.mobile.ajaxLinksEnabled = false;
+
 });
 
 
-$(document).on('load', '#page1', function(){
+$(document).on('pageinit', '#page1', function(){
+	if (localStorage['userToken']) {
+  		document.location.href = '#page_live';
+  		return;
+  	}
+	/*
 	$.ajax({
-		url: "http://www.google.com",
-		xhrFields: {
-			withCredentials: true
-		}
+		url: "http://www77.hattrick.org/Community/CHPP/NewDocs/Example.aspx?file=players&version=2.3&actionType=view"
 	})
 	.done(function() {
 		alert("done");
-	}).fail(function() {
+	}).fail(function(e) {
 		alert("fail");
 	});
-
+	*/
 });
 
-$(document).on('pageinit', '#page2', function(){
-    $('#match').text(localStorage['userToken']);
+$(document).on('pageinit', '#page_live', function(){
+	$.ajax({
+		url: "LiveMatchInfoRequest"
+	})
+	.done(function(xmlDoc) {
+		reponse = toJson(xmlDoc);
+		var matchText = buildMatch() || '';
+		$('#match').text(matchText);
+	}).fail(function(e) {
+		$('#match').text('No info');
+		$('#content').html('<p>No live match available...</p><p>Enter again when your time is playing a match</p>');
+	});
+
+    //$('#match').text(localStorage['userToken']);
 });
