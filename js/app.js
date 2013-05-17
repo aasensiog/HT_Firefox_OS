207d99fe4a25dd28a9270e4c2b8fa0d9976466ab
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-05-17 16:42:17 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -29,15 +29,43 @@ $(document).on('pageinit', '#index', function() {
 });
 
 $(document).on('pageinit', '#menu', function() {
-    getData(files.teamDetails).done(function(resp) {
-        alert(toJson(resp));
-    }).fail(function() {
-        alert('fail');
-    });
+
 });
 
 $(document).on('pageinit', '#authentication', function() {
     console.log('No tenemos el ok_access_token');
     console.log(localStorage.getItem('ok_oauth_token'));
     step1();
+});
+
+$(document).on('pageinit', '#matchList', function() {
+    getData(files.matches).done(function(resp) {
+        //TODO: Create a list of a with href=#live/match?id=matchId
+        alert(resp);
+    }).fail(function() {
+        alert('fail');
+    });
+});
+
+$(document).on('pageinit', '#team', function() {
+    getData(files.teamDetails).done(function(resp) {
+        alert(resp);
+    }).fail(function() {
+        alert('fail');
+    });
+});
+
+$(document).on('pageinit', '#match', function() {
+    var splitHref = window.location.href.split("?"),
+        idHref = splitHref[1].split("="),
+        matchId = idHref[1];
+
+});
+
+$(document).on('pageinit', '#live', function() {
+    var splitHref = window.location.href.split("?"),
+        idHref = splitHref[1].split("="),
+        matchId = idHref[1];
+
+
 });
